81b459680a1efc2a6d96171b4c7c92769130f5ec
Chris Feilen <cfeilen@microsoft.com>
2015-11-11 16:39:26 -0800

    Code cleanup

--- UdpFloodApp/UdpFloodApp/Program.cs
+++ UdpFloodApp/UdpFloodApp/Program.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UdpFloodApp
+﻿namespace UdpFloodApp
 {
+    using System;
     using System.Diagnostics;
     using System.Net;
     using System.Net.Sockets;
+    using System.Text;
 
-    static class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
             Console.WriteLine("UDP Flood application.");
             Console.WriteLine("Settings:");
@@ -40,7 +36,6 @@ namespace UdpFloodApp
 
             string header = $"{Environment.UserDomainName}\\{Environment.UserName} UDP Flood App.";
 
-            
             var ticksPerCycle = (int)(Stopwatch.Frequency / App.Default.NumberOfPacketsPerSecond);
 
             long count = 0;
@@ -49,10 +44,9 @@ namespace UdpFloodApp
             {
                 long nextPause = watch.ElapsedTicks + ticksPerCycle;
                 ++count;
-                long now = watch.ElapsedMilliseconds;
                 byte[] bytes = Encoding.ASCII.GetBytes($"{header}..{count}");
                 var newArray = new byte[App.Default.PacketSizeInBytes];
-                
+
                 Buffer.BlockCopy(bytes, 0, newArray, 0, bytes.Length);
                 client.Send(newArray, newArray.Length, endPoint);
 
@@ -61,11 +55,11 @@ namespace UdpFloodApp
                 {
                 }
 
-                if (lastSecond < (watch.ElapsedMilliseconds /1000))
+                if (lastSecond < (watch.ElapsedMilliseconds / 1000))
                 {
                     lastSecond = watch.ElapsedMilliseconds / 1000;
                     // This is a second boundry
-                    Console.Write("\rPackets per second: {0}               ", count/lastSecond);
+                    Console.Write("\rPackets per second: {0}               ", count / lastSecond);
                 }
             }
         }
